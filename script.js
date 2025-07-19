ba085cbb725751e898abea1192cc431594678baf
JonelGarcia29 <markjonelgarcia0@gmail.com>
2025-07-19 21:32:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,44 @@ document.querySelectorAll('nav ul li').forEach(link => {
         navMenu.classList.remove('active');
     });
 });
+
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const form = e.target;
+    
+    fetch(form.action, {
+        method: 'POST',
+        body: new FormData(form),
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+             Swal.fire({
+                title: 'Success!',
+                text: 'Your message has been sent successfully!',
+                icon: 'success',
+                background: '#333',
+                color: '#e0e0e0ff',
+                confirmButtonText: 'OK',
+                confirmButtonColor: '#005353ff'
+            });
+            form.reset();
+        } else {
+            throw new Error('Network response was not ok');
+        }
+    })
+    .catch(error => {
+        Swal.fire({
+            title: 'Error!',
+            text: 'There was a problem sending your message. Please try again later.',
+            icon: 'error',
+            background: '#333',
+            color: '#e0e0e0ff',
+            confirmButtonText: 'OK',
+            confirmButtonColor: '#005353ff'
+        });
+        console.error('Error:', error);
+    })
+});
